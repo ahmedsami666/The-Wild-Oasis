c2792286a0dffd908eaefbcfbbb6f9a6bd6d82c4
AhmedSami <71245088+ahmedsami666@users.noreply.github.com>
2024-09-01 15:54:31 +0300

    setting supabase database

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = import.meta.env.supabaseUrl
+
+const supabaseKey = import.meta.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase
