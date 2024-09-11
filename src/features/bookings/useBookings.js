fa98f5727a812ddd25cb518478d8fa5bb735bdb6
AhmedSami <71245088+ahmedsami666@users.noreply.github.com>
2024-09-11 13:57:31 +0300

    adding pagination

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -11,10 +11,12 @@ export function useBookings() {
     const sortByRaw = searchParams.get('sortBy') || 'startDate-desc'
     const [field, direction] = sortByRaw.split('-')
     const sortBy = {field, direction}
+    // PAGINATION
+    const page = !searchParams.get('page') ? 1 : Number(searchParams.get('page'))
 
-    const { isPending, data:bookings, error } = useQuery({
-        queryKey: ['bookings', filter, sortBy],
-        queryFn: () => getBookings({filter, sortBy})
+    const { isPending, data:{data: bookings, count} = {}, error } = useQuery({
+        queryKey: ['bookings', filter, sortBy, page],
+        queryFn: () => getBookings({filter, sortBy, page})
     })
-    return { isPending, bookings, error }
+    return { isPending, bookings, error, count }
 }
