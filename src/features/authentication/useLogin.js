6d0b09fea32bdbad3b59371da373b14b23839622
AhmedSami <71245088+ahmedsami666@users.noreply.github.com>
2024-09-14 23:27:42 +0300

    authorization protected route

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,13 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export function useLogin() {
+    const queryClient = useQueryClient()
     const navigate = useNavigate()
     const { mutate: login, isPending: isloading } = useMutation({
         mutationFn: ({ email, password }) => loginApi({email, password}),
-        onSuccess: () => {navigate('/dashboard')},
+        onSuccess: (user) => {
+            navigate('/dashboard')
+            queryClient.setQueriesData(['user'], user)
+        },
         onError: err => {
             console.log('ERROR', err.message)
             toast.error('Provided email or password are incorrect')
