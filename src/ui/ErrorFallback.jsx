b7cb6348fefc97234cf48494547c5c3e859783c2
AhmedSami <71245088+ahmedsami666@users.noreply.github.com>
2024-09-20 14:00:30 +0300

    error boudary handling

--- src/ui/ErrorFallback.jsx
+++ src/ui/ErrorFallback.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import Heading from "./Heading";
+import GlobleStyles from "../styles/GlobleStyles";
+import Button from "./Button";
 
 const StyledErrorFallback = styled.main`
   height: 100vh;
@@ -29,3 +32,18 @@ const Box = styled.div`
     color: var(--color-grey-500);
   }
 `;
+const ErrorFallback = ({ error, resetErrorBoundary }) => {
+  return (
+    <>
+      <GlobleStyles />
+      <StyledErrorFallback>
+        <Box>
+          <Heading as='h1'>Something went wrong</Heading>
+          <p>{error.message}</p>
+          <Button size='large' onClick={resetErrorBoundary}>Try again</Button>
+        </Box>
+      </StyledErrorFallback>
+    </>
+  )
+}
+export default ErrorFallback
